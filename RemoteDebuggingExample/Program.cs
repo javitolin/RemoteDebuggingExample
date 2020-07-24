24f989f9935b74662c81663fe0085413e6443357
JD <javitolin@Gmail.com>
2020-07-24 10:15:50 +0300

    Add project files.

---
+++ RemoteDebuggingExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteDebuggingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.MachineName);
+            Console.ReadLine();
+        }
+    }
+}
